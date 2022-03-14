648a3d02dc715e2e61804bdde45de023ac8303d9
hellodvh <hellodvh@gmail.com>
2022-03-14 10:51:18 +0200

    Add navbar top to pages. Add Google fonts links. Update page styles. Add Charts section.

--- TheInventory/wwwroot/js/site.js
+++ TheInventory/wwwroot/js/site.js
@@ -2,6 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+/* Apply active-link class to nav-link and update the css styling */
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('.nav-link').forEach(link => {
         if (link.getAttribute('href').toLowerCase() === location.pathname.toLowerCase()) {
