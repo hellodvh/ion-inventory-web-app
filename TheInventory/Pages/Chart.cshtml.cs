9be667dac7f94ea836096cde8d7af784bcf9a9d4
hellodvh <hellodvh@gmail.com>
2022-03-16 00:09:41 +0200

    Update Inventory page. Add Charts. Add Item Cards.

---
+++ TheInventory/Pages/Chart.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TheInventory.Pages
+{
+    public class ChartModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
